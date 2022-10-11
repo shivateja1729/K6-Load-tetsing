5f9c8e3f103871c64fbd2d243595133bbd0126f2
shivateja1729 <shivateja.dasi@the10xacademy.com>
2022-10-11 17:52:04 +0530

    added files

---
+++ index.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const async = require('async')
+const newman = require('newman')
+
+const PARALLEL_RUN_COUNT = 100
+
+const parametersForTestRun = {
+    collection: path.join(__dirname, 'postman/V2 Backend APIs.postman_collection.json'), // your collection
+    reporters: 'cli'
+};
+
+parallelCollectionRun = function (done) {
+    newman.run(parametersForTestRun, done);
+};
+
+let commands = []
+for (let index = 0; index < PARALLEL_RUN_COUNT; index++) {
+    commands.push(parallelCollectionRun);
+}
+
+// Runs the Postman sample collection thrice, in parallel.
+async.parallel(
+    commands,
+    (err, results) => {
+        err && console.error(err);
+
+        results.forEach(function (result) {
+            var failures = result.run.failures;
+            console.info(failures.length ? JSON.stringify(failures.failures, null, 2) :
+                `${result.collection.name} ran successfully.`);
+        });
+    });
